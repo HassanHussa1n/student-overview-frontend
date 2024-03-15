9c0070febf52c2dc99840c9151f63793a0b57867
Knut S Rostad <knut.rostad@no.experis.com>
2024-03-15 14:31:52 +0100

    Started

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,49 @@
-import { useState } from "react";
-
+import { useState, useEffect, createContext } from "react";
+import Login from "./Components/Login";
+import Main from "./Components/Main";
 import "./App.css";
 
+const MyContext = createContext();
+//Seperate context for login
+const LoginContext = createContext();
+const DUMMY_TEACHERS = [
+  { id: 1, name: "Dave" },
+  { id: 2, name: "Knut" },
+  { id: 3, name: "Hassan" },
+  { id: 4, name: "Mads" },
+];
+
 function App() {
+  //Dummy data for teacher array
+  const [teachers, setTeachers] = useState([DUMMY_TEACHERS]);
+  //Set currentuser = null if localStorage is not saved(logged out)
+  const [currentUser, setCurrentUser] = useState(null);
+  //Set the loggedin user the the last logged in
+  useEffect(() => {
+    const loggedInId = localStorage.getItem("loggedInId");
+    //If loggedInId exists, and users is set, find the user and set it as the logged in user
+    if (loggedInId && teachers) {
+      const foundUser = teachers.find(
+        (userItem) => Number(userItem.id) === Number(loggedInId)
+      );
+      setCurrentUser(foundUser);
+    }
+  }, [teachers]);
   return (
-    <>
-      <h1>Hello! This should work</h1>
-    </>
+    <div>
+      {currentUser ? (
+        <MyContext.Provider value={{ teachers, currentUser, setCurrentUser }}>
+          <Main />
+        </MyContext.Provider>
+      ) : (
+        <LoginContext.Provider
+          value={{ teachers, currentUser, setCurrentUser }}
+        >
+          <Login />
+        </LoginContext.Provider>
+      )}
+    </div>
   );
 }
 
-export default App;
+export { App, MyContext, LoginContext };
