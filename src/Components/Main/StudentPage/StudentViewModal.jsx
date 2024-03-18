fa5b286a90d60d5f643a2c53d06192b2fcb9a75e
Knut S Rostad <knut.rostad@no.experis.com>
2024-03-18 13:04:49 +0100

    added logo. Added modal for student

--- src/Components/Main/StudentPage/StudentViewModal.jsx
+++ src/Components/Main/StudentPage/StudentViewModal.jsx
@@ -3,14 +3,13 @@ export default function TeacherViewModal({
   isOpen,
   onClose,
   closeModal,
-  teacher,
+  student,
 }) {
   return (
     <Modal isOpen={isOpen} onRequestClose={onClose}>
       <h2>
-        {teacher.firstName} {teacher.lastName}
+        {student.firstName} {student.lastName}
       </h2>
-      <p>{teacher.email}</p>
 
       <button onClick={closeModal} className="close-btn">
         X
--- src/Components/Main/StudentPage/index.jsx
+++ src/Components/Main/StudentPage/index.jsx
@@ -1,13 +1,20 @@
 import { MyContext } from "../../../App.jsx";
-import { useContext } from "react";
+import { useContext} from "react";
 import StudentItem from "./StudentItem.jsx";
+
+
 export default function StudentPage() {
+
   const { students } = useContext(MyContext);
+
+  
 
   return (
     <ul className="student-list">
       {students.map((student, index) => {
-        return <StudentItem student={student} key={index} />;
+        return (
+          <StudentItem student={student} key={index} />
+        );
       })}
     </ul>
   );
