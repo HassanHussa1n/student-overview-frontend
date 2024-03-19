d4820b943f68448bb9978d3b740cf4aed4340f24
Hassan Hussain <hassanmehmodhussain@gmail.com>
2024-03-19 13:06:48 +0100

    modal

--- src/Components/Main/LecturePage/LectureItem.jsx
+++ src/Components/Main/LecturePage/LectureItem.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
 export default function LectureItem(props) {
-  const { user } = props;
+  const { post } = props;
   const [modalOpen, setModalOpen] = useState(false);
   const [formData, setFormData] = useState({});
 
@@ -27,31 +27,35 @@ export default function LectureItem(props) {
   return (
     <li>
     <div className="lecture">  
-    <h4>{user.title}</h4>
+    <h4>{post.title}</h4>
     <button onClick={openModal} className="view-btn">View</button>
     </div>
-    <Modal isOpen={modalOpen} onRequestClose={closeModal} appElement={document.getElementById("root")}>
+    <Modal isOpen={modalOpen} onRequestClose={closeModal} appElement={document.getElementById("root")}
+    className="custom-modal"
+    >
       <h2>Fill out the form</h2>
       <form>
         <input
           type="text"
           name="title"
+          value={post.title}
           placeholder="Title"
           onChange={handleInputChange}
         />
-        <input
+        <textarea
           type="textarea"
           name="content"
+          value={post.content}
           placeholder="Content"
           onChange={handleInputChange}
         />
-        <button type="button" onClick={handleSubmit}>
+        <button type="button" onClick={handleSubmit} className="submit-btn">
           Submit
         </button>
-        <button type="button" onClick={closeModal} className="close-btn">
-          x
+        <button type="button" onClick={closeModal || deleteLecture} className="close-btn">
+          <span className="close-btn-text">X</span>
         </button>
-        <button type="button" onClick={deleteLecture}>Delete</button>
+        
       </form>
     </Modal>
   </li>
