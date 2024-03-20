8a8f4557f75e78955a514caf14be93069fff58dc
Hassan Hussain <hassanmehmodhussain@gmail.com>
2024-03-20 09:06:12 +0100

    Knut needs

--- src/Components/Main/TeacherPage/TeacherViewModal.jsx
+++ src/Components/Main/TeacherPage/TeacherViewModal.jsx
@@ -10,11 +10,28 @@ export default function TeacherViewModal({
       isOpen={isOpen}
       onRequestClose={onClose}
       appElement={document.getElementById("root")}
+      className="custom-modal"
     >
       <h2>
         {teacher.firstName} {teacher.lastName}
       </h2>
-      <p>{teacher.email}</p>
+      <p>Email: {teacher.email}</p>
+      <p>Phone Number: {teacher.phone}</p>
+      <ul>
+      {teacher.classrooms.map((classroom, index) => {
+            return (
+              <>
+              <p>Classrooms</p>
+            
+              <li
+                key={index}
+                type="text"
+                name={`classroom${index}`}
+              >{classroom.name}</li>
+              </>
+            );
+          })}
+        </ul>
 
       <button onClick={closeModal} className="close-btn">
         <span className="close-btn-text">X</span>
